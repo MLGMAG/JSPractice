75e0e2e985894fd2325f0185714f6780aa6b5d19
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-07-28 17:18:09 +0300

    [Formating] Reformat file

--- jsFiles/immutability.js
+++ jsFiles/immutability.js
@@ -5,11 +5,10 @@ const leftBorderWidth = 1; // if we try to change it we receive exception
 
 console.log(number);
 
-const obj = {       // can change constant object state!
+const obj = { // can change constant object state!
     a: 50
 };
 
 obj.a = 10;
 
 console.log(obj.a);
-
