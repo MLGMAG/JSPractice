d3d8bd81714d9b44d435120b61daa2bf8a295db6
Akhmedov, Mahomed <mahomed_akhmedov@epam.com>
2021-07-28 09:49:36 +0300

    [BasicInput] Practice usage of alert/confirm/prompt

---
+++ userInput/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+alert("Hello!");
+
+let confirmAnswer = confirm("Are you developer?");
+
+let promtAnswer = prompt("How old are you?", "");
+
+console.log(`Is user a developer: ${confirmAnswer}`);
+console.log(`User is ${promtAnswer} years old`);
